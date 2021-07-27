1d31bb8777704117400f3054370f193f165a9137
anthien30 <tnguyen13099@gmail.com>
2021-07-27 12:47:30 -0700

    trying to find a bug

--- services/passport.js
+++ services/passport.js
@@ -25,15 +25,17 @@ passport.use(
       proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            done(null, existingUser);
+          } else {
+            new User({ googleId: profile.id })
+              .save()
+              .then((user) => done(null, user));
+          }
+        })
+        .catch((err) => console.log(err));
     }
   )
 );
